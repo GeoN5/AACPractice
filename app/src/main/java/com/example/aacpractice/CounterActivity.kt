8f276f59d3353bd5c71628da2483f319f636ce3e
GeoN5 <sgepyh2916@naver.com>
2019-05-23 22:49:32 +0900

    2 : ViewModel 추가 적용

--- app/src/main/java/com/example/aacpractice/CounterActivity.kt
+++ app/src/main/java/com/example/aacpractice/CounterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aacpractice
 
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,20 +8,23 @@ import com.example.aacpractice.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
 
-    private var counter:Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //데이터 바인딩
         val binding : ActivityCounterBinding = DataBindingUtil.setContentView(this,R.layout.activity_counter)
+        //뷰모델
+        val viewModel = ViewModelProviders.of(this).get(CounterViewModel::class.java)
 
-        binding.countTextView.text = "0"
+        binding.countTextView.text = viewModel.counter.toString()
 
         binding.addFab.setOnClickListener {
-            binding.countTextView.text = counter++.toString()
+            viewModel.counter++
+            binding.countTextView.text = viewModel.counter.toString()
         }
 
         binding.subFab.setOnClickListener {
-            binding.countTextView.text = counter--.toString()
+            viewModel.counter--
+            binding.countTextView.text = viewModel.counter.toString()
         }
 
     }
---
+++ app/src/main/java/com/example/aacpractice/CounterViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.aacpractice
+
+import android.arch.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+
+    var counter:Int = 0
+
+}
